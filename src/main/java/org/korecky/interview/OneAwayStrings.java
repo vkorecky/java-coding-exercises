850849529414d8c8caf6e4388271f360f051e30f
vkorecky <vkorecky@korecky.org>
2020-03-21 17:47:21 +0100

    Added solution of problem 'One Away Strings'

--- src/main/java/org/korecky/interview/OneAwayStrings.java
+++ src/main/java/org/korecky/interview/OneAwayStrings.java
@@ -10,6 +10,39 @@ public class OneAwayStrings {
 	//   - "a" and "a" are one away (changing the only character 'a' to the equivalent character 'a').
 	//   - "abc" and "bcc" are NOT one away. (They are two operations away.)
 	public static Boolean isOneAway(String s1, String s2) {
-		return false;
+		if (Math.abs(s1.length() - s2.length()) > 1) {
+			return false;
+		}
+
+		char[] longerArray;
+		char[] shorterArray;
+
+		if (s1.length() > s2.length()) {
+			longerArray = s1.toCharArray();
+			shorterArray = s2.toCharArray();
+		} else {
+			longerArray = s2.toCharArray();
+			shorterArray = s1.toCharArray();
+		}
+
+		boolean operationApplied = false;
+		int offset = 0;
+		for (int i = 0; i < shorterArray.length; i++) {
+			char firstChar = longerArray[i + offset];
+			char secondChar = shorterArray[i];
+			if (firstChar != secondChar) {
+				if (operationApplied)
+					return false;
+				if (secondChar == longerArray[i+1]) {
+					// add/delete operation
+					operationApplied = true;
+					offset = 1;
+				} else if (longerArray[i+1] == shorterArray[i+1]){
+					operationApplied = true;
+				} else
+					return false;
+			}
+		}
+		return true;
 	}
 }
