60f015a10fc0f4203529e2866d1b5eb95abef3c6
vkorecky <vkorecky@korecky.org>
2020-04-22 21:28:57 +0200

    Added solution for Rotation with copy of array

--- src/main/java/org/korecky/interview/Rotating2DArrayBy90Degrees.java
+++ src/main/java/org/korecky/interview/Rotating2DArrayBy90Degrees.java
@@ -15,6 +15,11 @@ public class Rotating2DArrayBy90Degrees {
 
 	public static int[][] rotate(int[][] a, int n) {
 		int[][] rotated = new int[n][n];
+		for (int i=0; i<n; i++){
+			for (int j=0; j<n; j++){
+				rotated[j][(n-1)-i] = a[i][j];
+			}
+		}
 		return rotated;
 	}
 
